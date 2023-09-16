30fdb5a25df241508236c8bc19c07c5d5638006f
sara219 <sara97abuawwad@gmail.com>
2023-09-16 21:32:33 +0300

    fix issues and circleCI

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'sunkmi',
   e2e: {
+    viewportWidth: 1440,
+    viewportHeight: 900,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,10 +3,12 @@
 describe("Sign Up Accessibility test suite", () => {
   before(() => {
     cy.visit("http://localhost:3000");
+    // inject axe accessibility testing library into the web page.
     cy.injectAxe();
   });
 
   it("Check entire page for a11y issues", () => {
+    // check the accessibility of the entire page using axe library. it runs series of accessibility checks on the page and report any violations or issues.
     cy.checkA11y();
   });
 });
